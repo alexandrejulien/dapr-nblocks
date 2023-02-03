dca099d1a0c68c5fdd9bbe8c73fa98cade5e6fa4
Alexandre JULIEN <mail@alexandrejulien.fr>
2023-02-03 19:18:15 +0100

    Adding unit test.

---
+++ tests/DaprNBlocks.Core.Tests/UniqueIdentifierTests.cs
@@ -0,0 +1,21 @@
+﻿using DaprNBlocks.Core.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaprNBlocks.Core.Tests
+{
+    [TestClass]
+    public class UniqueIdentifierTests
+    {
+        [TestMethod]
+        public void GivenWhenNeededAUniqueIdentifier()
+        {
+            Check.That(Guid.Parse(UniqueIdentifier.New())).IsInstanceOf<Guid>();
+        }
+    }
+}
